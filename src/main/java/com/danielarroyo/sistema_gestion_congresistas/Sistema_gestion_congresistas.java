d05b584b3537d1d60074f570ea2b99802cbae12e
DANIEL ARROYO <arroyosteven937@gmail.com>
2025-06-14 17:33:50 -0500

    creacion del proyecto

---
+++ src/main/java/com/danielarroyo/sistema_gestion_congresistas/Sistema_gestion_congresistas.java
@@ -0,0 +1,9 @@
+package com.danielarroyo.sistema_gestion_congresistas;
+
+
+public class Sistema_gestion_congresistas {
+
+    public static void main(String[] args) {
+        System.out.println("sistema congresistas");
+    }
+}
